62a90ad1ecfa7b1b1e1e1789b1d8eb32a09d3951
Ryan Shim <jhshim@robotis.com>
2019-09-04 14:47:07 +0900

    control race first version done

---
+++ turtlebot3_gazebo2/models/turtlebot3_controlrace/gazebo_controlrace_plugin/build/CMakeFiles/obstacle_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/obstacle_1.dir/obstacle_1.cc.o"
+  "libobstacle_1.pdb"
+  "libobstacle_1.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/obstacle_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ turtlebot3_gazebo2/models/turtlebot3_controlrace/gazebo_controlrace_plugin/build/CMakeFiles/obstacle_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/obstacle_2.dir/obstacle_2.cc.o"
+  "libobstacle_2.pdb"
+  "libobstacle_2.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/obstacle_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
