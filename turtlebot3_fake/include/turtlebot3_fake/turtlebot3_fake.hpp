d08872089f0e405dc429d25f0f2f120c93865b6a
Ryan Shim <jhshim@robotis.com>
2019-09-06 10:27:22 +0900

    fake node ported

---
+++ turtlebot3_fake/include/turtlebot3_fake/turtlebot3_fake.hpp
@@ -0,0 +1,115 @@
+/*******************************************************************************
+* Copyright 2019 ROBOTIS CO., LTD.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+
+/* Authors: Yoonseok Pyo */
+
+#ifndef TURTLEBOT3_FAKE_HPP_
+#define TURTLEBOT3_FAKE_HPP_
+
+#include <math.h>
+
+#include <rclcpp/rclcpp.hpp>
+#include <chrono>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_ros/transform_broadcaster.h>
+
+#include "std_msgs/msg/int32.hpp"
+#include "sensor_msgs/msg/imu.hpp"
+#include "sensor_msgs/msg/joint_state.hpp"
+#include "geometry_msgs/msg/vector3.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+#include "turtlebot3_msgs/msg/sensor_state.hpp"
+
+#define WHEEL_RADIUS                    0.033     // meter
+
+#define LEFT                            0
+#define RIGHT                           1
+
+#define MAX_LINEAR_VELOCITY             0.22   // m/s
+#define MAX_ANGULAR_VELOCITY            2.84   // rad/s
+#define VELOCITY_STEP                   0.01   // m/s
+#define VELOCITY_LINEAR_X               0.01   // m/s
+#define VELOCITY_ANGULAR_Z              0.1    // rad/s
+#define SCALE_VELOCITY_LINEAR_X         1
+#define SCALE_VELOCITY_ANGULAR_Z        1
+
+#define DEG2RAD(x)                      (x * 0.01745329252)  // *PI/180
+#define RAD2DEG(x)                      (x * 57.2957795131)  // *180/PI
+
+#define TORQUE_ENABLE                   1       // Value for enabling the torque of motor
+#define TORQUE_DISABLE                  0       // Value for disabling the torque of motor
+
+class Turtlebot3Fake : public rclcpp::Node
+{
+ public:
+  Turtlebot3Fake();
+  ~Turtlebot3Fake();
+  
+  bool update_callback();
+
+ private:
+  /************************************************************
+  ** Init Functions
+  ************************************************************/
+  void init_parameters();
+  void init_variables();
+
+  // ROS Time
+  rclcpp::Time last_cmd_vel_time_;
+  rclcpp::Time prev_update_time_;
+
+  // ROS Topic Publishers
+  rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr joint_states_pub_;
+  rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_pub_;
+
+  // ROS Topic Subscribers
+  rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_sub_;
+
+  rclcpp::TimerBase::SharedPtr update_timer_;
+
+  sensor_msgs::msg::JointState joint_states_;
+  nav_msgs::msg::Odometry odom_;
+  
+  std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
+
+  std::string robot_model_;
+  double wheel_speed_cmd_[2];
+  double goal_linear_velocity_;
+  double goal_angular_velocity_;
+  double cmd_vel_timeout_;
+
+  float  odom_pose_[3];
+  float  odom_vel_[3];
+  double pose_cov_[36];
+
+  std::string joint_states_name_[2];
+
+  double last_position_[2];
+  double last_velocity_[2];
+
+  double wheel_seperation_;
+  double turning_radius_;
+  double robot_radius_;
+
+  // Function prototypes
+  void command_velocity_callback(const geometry_msgs::msg::Twist::SharedPtr cmd_vel_msg);
+  bool update_odometry(rclcpp::Duration diff_time);
+  void update_joint(void);
+  void update_tf(geometry_msgs::msg::TransformStamped & odom_tf);
+};
+
+#endif // TURTLEBOT3_FAKE_HPP_
---
+++ turtlebot3_fake/src/turtlebot3_fake.cpp
@@ -0,0 +1,296 @@
+/*******************************************************************************
+* Copyright 2019 ROBOTIS CO., LTD.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+
+/* Authors: Yoonseok Pyo */
+
+#include "turtlebot3_fake/turtlebot3_fake.hpp"
+
+using namespace std::chrono_literals;
+
+Turtlebot3Fake::Turtlebot3Fake()
+: Node("turtlebot3_fake_node")
+{
+  /************************************************************
+  ** Initialise ROS Parameters
+  ************************************************************/
+  init_parameters();
+
+  /************************************************************
+  ** Initialise Variables
+  ************************************************************/
+  init_variables();
+
+  /************************************************************
+  ** Initialise ROS Publishers, Subscribers
+  ************************************************************/
+  // Initialise Publishers
+  joint_states_pub_ = this->create_publisher<sensor_msgs::msg::JointState>("joint_states", 100);
+  odom_pub_ = this->create_publisher<nav_msgs::msg::Odometry>("odom", 100);
+
+  // Initialise Subscribers
+  cmd_vel_sub_ = this->create_subscription<geometry_msgs::msg::Twist>(
+    "cmd_vel", 100, std::bind(&Turtlebot3Fake::command_velocity_callback, this, std::placeholders::_1));
+
+  /************************************************************
+  ** Start Update Thread
+  ************************************************************/
+  update_timer_ = this->create_wall_timer(30ms, std::bind(&Turtlebot3Fake::update_callback, this));
+
+  RCLCPP_INFO(this->get_logger(), "Turtlebot3 Fake Node Initialised");
+}
+
+Turtlebot3Fake::~Turtlebot3Fake() 
+{  
+  RCLCPP_INFO(this->get_logger(), "Turtlebot3 Fake Node Terminated");
+}
+
+/********************************************************************************
+** Init Functions
+********************************************************************************/
+void Turtlebot3Fake::init_parameters()
+{
+  // Declare parameters that may be set on this node
+  this->declare_parameter(
+    "tb3_model",
+    rclcpp::ParameterValue(std::string("")),
+    rcl_interfaces::msg::ParameterDescriptor());
+  this->declare_parameter(
+    "wheel_left_joint_name",
+    rclcpp::ParameterValue(std::string("wheel_left_joint")),
+    rcl_interfaces::msg::ParameterDescriptor());
+  this->declare_parameter(
+    "wheel_right_joint_name",
+    rclcpp::ParameterValue(std::string("wheel_right_joint")),
+    rcl_interfaces::msg::ParameterDescriptor());
+  this->declare_parameter(
+    "joint_states_frame",
+    rclcpp::ParameterValue(std::string("base_footprint")),
+    rcl_interfaces::msg::ParameterDescriptor());
+  this->declare_parameter(
+    "odom_frame",
+    rclcpp::ParameterValue(std::string("odom")),
+    rcl_interfaces::msg::ParameterDescriptor());
+  this->declare_parameter(
+    "base_frame",
+    rclcpp::ParameterValue(std::string("base_footprint")),
+    rcl_interfaces::msg::ParameterDescriptor());
+
+  // Get parameter from yaml
+  this->get_parameter("tb3_model", robot_model_);
+  this->get_parameter("wheel_left_joint_name", joint_states_name_[LEFT]);
+  this->get_parameter("wheel_right_joint_name", joint_states_name_[RIGHT]);
+  this->get_parameter("joint_states_frame", joint_states_.header.frame_id);
+  this->get_parameter("odom_frame", odom_.header.frame_id);
+  this->get_parameter("base_frame", odom_.child_frame_id);
+}
+
+void Turtlebot3Fake::init_variables()
+{
+  if (!robot_model_.compare("burger"))
+  {
+    wheel_seperation_ = 0.160;
+    turning_radius_   = 0.080;
+    robot_radius_     = 0.105;
+  }
+  else if (!robot_model_.compare("waffle") || !robot_model_.compare("waffle_pi"))
+  {
+    wheel_seperation_ = 0.287;
+    turning_radius_   = 0.1435;
+    robot_radius_     = 0.220;
+  }
+
+  // Initialise Variables
+  wheel_speed_cmd_[LEFT]  = 0.0;
+  wheel_speed_cmd_[RIGHT] = 0.0;
+  goal_linear_velocity_   = 0.0;
+  goal_angular_velocity_  = 0.0;
+  cmd_vel_timeout_        = 1.0;
+  last_position_[LEFT]    = 0.0;
+  last_position_[RIGHT]   = 0.0;
+  last_velocity_[LEFT]    = 0.0;
+  last_velocity_[RIGHT]   = 0.0;
+
+  double pcov[36] = { 0.1,   0,   0,   0,   0, 0,
+                        0, 0.1,   0,   0,   0, 0,
+                        0,   0, 1e6,   0,   0, 0,
+                        0,   0,   0, 1e6,   0, 0,
+                        0,   0,   0,   0, 1e6, 0,
+                        0,   0,   0,   0,   0, 0.2};
+  memcpy(&(odom_.pose.covariance),pcov,sizeof(double)*36);
+  memcpy(&(odom_.twist.covariance),pcov,sizeof(double)*36);
+
+  odom_pose_[0] = 0.0;
+  odom_pose_[1] = 0.0;
+  odom_pose_[2] = 0.0;
+
+  odom_vel_[0] = 0.0;
+  odom_vel_[1] = 0.0;
+  odom_vel_[2] = 0.0;
+
+  joint_states_.name.push_back(joint_states_name_[LEFT]);
+  joint_states_.name.push_back(joint_states_name_[RIGHT]);
+  joint_states_.position.resize(2, 0.0);
+  joint_states_.velocity.resize(2, 0.0);
+  joint_states_.effort.resize(2, 0.0);
+
+  rclcpp::Clock clock(RCL_SYSTEM_TIME);
+  prev_update_time_ = clock.now();
+}
+
+/********************************************************************************
+** Callback Functions for ROS Subscribers
+********************************************************************************/
+void Turtlebot3Fake::command_velocity_callback(const geometry_msgs::msg::Twist::SharedPtr cmd_vel_msg)
+{
+  rclcpp::Clock clock(RCL_SYSTEM_TIME);
+  last_cmd_vel_time_ = clock.now();
+
+  goal_linear_velocity_  = cmd_vel_msg->linear.x;
+  goal_angular_velocity_ = cmd_vel_msg->angular.z;
+
+  wheel_speed_cmd_[LEFT]  = goal_linear_velocity_ - (goal_angular_velocity_ * wheel_seperation_ / 2);
+  wheel_speed_cmd_[RIGHT] = goal_linear_velocity_ + (goal_angular_velocity_ * wheel_seperation_ / 2);
+}
+
+/********************************************************************************
+** Functions related to update_callback 
+********************************************************************************/
+bool Turtlebot3Fake::update_callback()
+{
+  rclcpp::Clock clock(RCL_SYSTEM_TIME);
+  rclcpp::Time time_now = clock.now();
+  rclcpp::Duration step_time = time_now - prev_update_time_;
+  prev_update_time_ = time_now;
+
+  // zero-ing after timeout
+  if((time_now - last_cmd_vel_time_).seconds() > cmd_vel_timeout_)
+  {
+    wheel_speed_cmd_[LEFT]  = 0.0;
+    wheel_speed_cmd_[RIGHT] = 0.0;
+  }
+
+  // odom
+  update_odometry(step_time);
+  odom_.header.stamp = time_now;
+  odom_pub_->publish(odom_);
+
+  // joint_states
+  update_joint();
+  joint_states_.header.stamp = time_now;
+  joint_states_pub_->publish(joint_states_);
+
+  // tf
+  geometry_msgs::msg::TransformStamped odom_tf;
+  update_tf(odom_tf);
+  tf_broadcaster_->sendTransform(odom_tf);
+
+  return true;
+}
+
+bool Turtlebot3Fake::update_odometry(rclcpp::Duration diff_time)
+{
+  double wheel_l, wheel_r; // rotation value of wheel [rad]
+  double delta_s, delta_theta;
+  double v[2], w[2];
+
+  wheel_l = wheel_r     = 0.0;
+  delta_s = delta_theta = 0.0;
+
+  v[LEFT]  = wheel_speed_cmd_[LEFT];
+  w[LEFT]  = v[LEFT] / WHEEL_RADIUS;  // w = v / r
+  v[RIGHT] = wheel_speed_cmd_[RIGHT];
+  w[RIGHT] = v[RIGHT] / WHEEL_RADIUS;
+
+  last_velocity_[LEFT]  = w[LEFT];
+  last_velocity_[RIGHT] = w[RIGHT];
+
+  wheel_l = w[LEFT]  * diff_time.seconds();
+  wheel_r = w[RIGHT] * diff_time.seconds();
+
+  if(isnan(wheel_l))
+  {
+    wheel_l = 0.0;
+  }
+
+  if(isnan(wheel_r))
+  {
+    wheel_r = 0.0;
+  }
+
+  last_position_[LEFT]  += wheel_l;
+  last_position_[RIGHT] += wheel_r;
+
+  delta_s     = WHEEL_RADIUS * (wheel_r + wheel_l) / 2.0;
+  delta_theta = WHEEL_RADIUS * (wheel_r - wheel_l) / wheel_seperation_;
+
+  // compute odometric pose
+  odom_pose_[0] += delta_s * cos(odom_pose_[2] + (delta_theta / 2.0));
+  odom_pose_[1] += delta_s * sin(odom_pose_[2] + (delta_theta / 2.0));
+  odom_pose_[2] += delta_theta;
+
+  // compute odometric instantaneouse velocity
+  odom_vel_[0] = delta_s / diff_time.seconds();     // v
+  odom_vel_[1] = 0.0;
+  odom_vel_[2] = delta_theta / diff_time.seconds(); // w
+
+  odom_.pose.pose.position.x = odom_pose_[0];
+  odom_.pose.pose.position.y = odom_pose_[1];
+  odom_.pose.pose.position.z = 0;
+
+  tf2::Quaternion q;  
+  q.setRPY(0, 0, odom_pose_[2]);
+
+  odom_.pose.pose.orientation.x = q.x();
+  odom_.pose.pose.orientation.y = q.y();
+  odom_.pose.pose.orientation.z = q.z();
+  odom_.pose.pose.orientation.w = q.w();
+
+  // We should update the twist of the odometry
+  odom_.twist.twist.linear.x  = odom_vel_[0];
+  odom_.twist.twist.angular.z = odom_vel_[2];
+
+  return true;
+}
+
+void Turtlebot3Fake::update_joint(void)
+{
+  joint_states_.position[LEFT]  = last_position_[LEFT];
+  joint_states_.position[RIGHT] = last_position_[RIGHT];
+  joint_states_.velocity[LEFT]  = last_velocity_[LEFT];
+  joint_states_.velocity[RIGHT] = last_velocity_[RIGHT];
+}
+
+void Turtlebot3Fake::update_tf(geometry_msgs::msg::TransformStamped& odom_tf)
+{
+  odom_tf.header = odom_.header;
+  odom_tf.child_frame_id = odom_.child_frame_id;
+  odom_tf.transform.translation.x = odom_.pose.pose.position.x;
+  odom_tf.transform.translation.y = odom_.pose.pose.position.y;
+  odom_tf.transform.translation.z = odom_.pose.pose.position.z;
+  odom_tf.transform.rotation = odom_.pose.pose.orientation;
+}
+
+/*****************************************************************************
+** Main
+*****************************************************************************/
+int main(int argc, char **argv)
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<Turtlebot3Fake>());
+  rclcpp::shutdown();
+
+  return 0;
+}
