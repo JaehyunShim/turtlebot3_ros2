dcddab84e87b6b46eda12601cd69fbeb3058b6ab
Ryan Shim <jhshim@robotis.com>
2019-09-09 17:56:06 +0900

    tb3 basic examples initial uploade

---
+++ turtlebot3_example/setup.py
@@ -0,0 +1,42 @@
+import glob
+import os
+
+from setuptools import find_packages
+from setuptools import setup
+
+package_name = 'turtlebot3_example'
+
+setup(
+    name=package_name,
+    version='',
+    packages=find_packages(exclude=[]),
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=[
+        'setuptools',
+    ],
+    zip_safe=True,
+    author='Ryan Shim',
+    author_email='jhshim@robotis.com',
+    maintainer='Pyo',
+    maintainer_email='pyo@robotis.com',
+    keywords=['ROS'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description=(
+        'Teleoperation node using keyboard for TurtleBot3.'
+    ),
+    license='Apache License, Version 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'teleop_keyboard = turtlebot3_teleop.script.teleop_keyboard:main'
+        ],
+    },
+)
